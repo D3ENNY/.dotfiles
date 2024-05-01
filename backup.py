28756c3b006f1fe622ac2ac772e21f15dc881ca2
D3ENNY <denysraimondi06@gmail.com>
2024-05-01 15:50:12 +0200

    change config file method backup.py

--- backup.py
+++ backup.py
@@ -12,23 +12,103 @@ from glob import glob
 ##########################
 class System: 
     HOME_DIR = expanduser('~')
-    CONFIG_DIR = join(HOME_DIR, '.config')
+    CONFIG_DIR = f"{HOME_DIR}/.config"
     USR_SHARE_DIR = '/usr/share'
-    IMG_DIR = join(HOME_DIR, 'Immagini')
-    LOCAL_SHARE_DIR = join(HOME_DIR, '.local', 'share')
+    IMG_DIR = f"{HOME_DIR}/Immagini"
+    LOCAL_SHARE_DIR = f"{HOME_DIR}/.local/share"
     
-class Kde(System):
-    CONFIG_FILES = ['albafetch/', 'autostart/', 'btop/', 'dolphinrc', 'fish/', 'flameshot/', 'flameshotrc', 'fontconfig/', 'kdedefaults/', 
-                    'konsolerc', 'krunnerrc', 'kscreenlockerrc', 'Kvantum/', 'kwinrc', 'latte', 'neofetch/', 
-                    'plasma-org.kde.plasma.desktop-appletsrc', 'plasmarc', 'powermanagementprofilesrc', 'xsettingsd/', 'yakuakerc',] 
-    
-    HOME_FILES = ['.alias.conf', '.bash_profile', '.bashrc', '.bashrc1', '.logseq/', '.screenlayout/']
-    
-    USR_SHARE_FILE = ['defaultbg/']
-    
-    IMG_FILE = ['desktop.png', 'bg konsole.png', 'opera.jpg', 'icon/']
-    
-    LOCAL_SHARE_FILE = ['applications/', 'dolphin/', 'konsole/', 'plasma_icons/', 'uptime-record']
+configs = {
+    "kde" : {
+        "path": {
+            System.CONFIG_DIR : [
+                'albafetch/',
+                'autostart/', 
+                'btop/', 
+                'dolphinrc',
+                'fish/',
+                'flameshot/', 
+                'flameshotrc',
+                'fontconfig/',
+                'kdedefaults/',
+                'konsolerc',
+                'krunnerrc',
+                'kscreenlockerrc',
+                'Kvantum/', 
+                'kwinrc',
+                'latte',
+                'neofetch/', 
+                'plasma-org.kde.plasma.desktop-appletsrc',
+                'plasmarc', 'powermanagementprofilesrc',
+                'xsettingsd/', 
+                'yakuakerc'
+            ],
+            System.HOME_DIR :[
+                '.alias.conf',
+                '.bash_profile',
+                '.bashrc',
+                '.bashrc1',
+                '.logseq/',
+                '.screenlayout/'
+            ],
+            System.USR_SHARE_DIR : [
+                'defaultbg/'
+            ],
+            System.IMG_DIR : [
+                'desktop.png',
+                'bg konsole.png',
+                'opera.jpg',
+                'icon/'
+            ],
+            System.LOCAL_SHARE_DIR : [
+                'applications/', 
+                'dolphin/',
+                'konsole/',
+                'plasma_icons/',
+                'uptime-record'
+            ]
+        },
+        "msg" : lambda : print("saving KDE Plasma's config")
+    },
+    "hyprland" : {
+        "path" :  {
+            System.CONFIG_DIR : [
+                "albafetch/",
+                "btop/", 
+                "Code/",
+                "discord-screenaudio/",
+                "fish/",
+                "gtk-2.0/",
+                "gtk-3.0/",
+                "hypr/",
+                "hyprland-autoname-workspaces/",
+                "kitty/",
+                "Logseq/",
+                "neofetch/",
+                "nvim/",
+                "pipes-rs/",
+                "rofi/",
+                "scripts/",
+                "thunar/",
+                "waybar/",
+                "user-dirs.dirs",
+                "user-dirs.locale"
+            ],
+            System.HOME_DIR : [
+                ".logseq/",
+                ".alias.conf",
+                ".bash_profile",
+                ".bashrc"
+            ],
+            System.LOCAL_SHARE_DIR : [
+                "sddm/"
+            ],
+            System.IMG_DIR : [
+                "wallpaper"
+            ]
+        },
+        "msg" : lambda : print("saving Hyprland's config")
+    }
+}
 
 repo = Repo('./')
 origin = repo.remote('origin')
@@ -36,71 +116,54 @@ origin = repo.remote('origin')
 #        FUNCTIONS       #
 ##########################
 
-def enhanced_copy(src, dst):
+def enhanced_copy(src, dst, key):
     try:
         if isdir(src):
             copytree(src, dst, symlinks=True, ignore=None, copy_function=copy2, ignore_dangling_symlinks=False, dirs_exist_ok=False)
         else:
             makedirs(dirname(dst), exist_ok=True)
             copy2(src, dst)
-    except (FileNotFoundError, FileExistsError):
-        with open(join("kde", "log.txt"), 'a+') as log:
-            log.write(f'src: {src} - dst: {dst}\n')
-        
-        
-def backupCategory(src, dst, files):
-    with tqdm(total=len(files)) as progressBar:
-        for i in files:
-            enhanced_copy(join(src, i), join(dst, i))
-            sleep(0.01)
-            progressBar.update(1)
+    except (FileNotFoundError, FileExistsError) as err:
+        with open(f"{key}/log.txt", 'a+') as log:
+            log.write(f'src: {src} - dst: {dst}\n{err}\n\n')
             
 def commit(src, msg):
     for i in glob(join(src, '.*')) + glob(join(src, '*')):
         repo.index.add(i)
         print(f'{i} folder added to local repository')
         repo.index.commit(msg)
-    print("commits effettuated")
+    print("commits mades")
     
-
-def kde():
-    
-    rmtree('kde/', ignore_errors=True)
-    makedirs('kde', exist_ok=True)
-    
-    print('start saving ~/.config folder')
-    backupCategory(Kde.CONFIG_DIR, join('kde', '.config'), Kde.CONFIG_FILES)
+def apply_config(key):
+    config = configs.get(key)
+    if config:
+        config['msg']
+        paths = config['path'] 
+        
+        rmtree(key, ignore_errors=True)
+        makedirs(key, exist_ok=True)
+        
+        for path in paths:
+            print(f"start saving {path} folder")
             
-    print('start saving home (~/) folder')
-    backupCategory(Kde.HOME_DIR, join('kde', 'home'), Kde.HOME_FILES)
+            with tqdm(total=len(paths[path])) as progressBar:
+                for file in paths[path]:
+                    enhanced_copy(f"{path}/{file}", f"{key}/{path[1:]}", key)
+                    sleep(0.01)
+                    progressBar.update(1)
+        commit(key, f"update {key} .dotfile")
     
-    print('start saving /usr/share folder')
-    backupCategory(Kde.USR_SHARE_DIR, f'kde/{Kde.USR_SHARE_DIR}', Kde.USR_SHARE_FILE)
-            
-    print('start saving ~/Immagini folder')
-    backupCategory(Kde.IMG_DIR, join('kde', 'Immagini'), Kde.IMG_FILE)
-            
-    print('start saving ~/.local/share folder')
-    backupCategory(Kde.LOCAL_SHARE_DIR, join('kde', '.local', 'share'), Kde.LOCAL_SHARE_FILE)
-    
-    print('\n\n.dotfile saved')
-    
-            
-#TODO implementare backup hyprland
-def hyprland():
-    print('TODO')
     
 ##########################
 #         STARTUP        #
 ##########################
-    
+#TODO: DA AUTOMATIZZARE
 choise = int(input('do you want to update KDE config or hyprland config?\n[1] KDE plasma\n[2] HyprlandWM\n'))
 system('clear')
 if choise == 1:
-    kde()
-    commit("kde/", 'update KDE .dotfile')
+    apply_config("kde")
 elif choise == 2:
-    hyprland()
+    apply_config("hyprland")
 else:
     print('unhandled input')
     exit(1)
